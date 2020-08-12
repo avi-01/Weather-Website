d6dbbd6ccea21625442efaeeef66cef1d89b3c47
avi-01 <avnishmay@gmail.com>
2020-08-12 12:05:11 +0530

    changes to package.json location

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('../Public'))
+app.use(express.static('Public'))
 app.use(express.json());
 // app.use(cors());
 
